e01a0ff2e81c596598484dc22734f7be505d5677
Keane O'Kelley <keane.m.okelley@gmail.com>
2021-02-11 11:13:47 -0500

    start serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User, Group
+from django.utils import timezone
 from rest_framework import serializers
+
+from core.models import Project
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +15,19 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    created_at = serializers.DateTimeField(default=timezone.now)
+    image = serializers.ImageField(allow_null=True)
+
+    def create(self, validated_data):
+        return Project.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.image = validated_data.get("image", instance.image)
+        instance.save()
+        return instance
