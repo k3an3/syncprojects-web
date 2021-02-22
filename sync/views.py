59898f7ae4820edb66fe6c563cd5af2950064e02
Keane O'Kelley <me@keane.space>
2021-02-21 22:50:12 -0500

    Secure that route

--- sync/views.py
+++ sync/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from api.utils import get_tokens_for_user
 from sync.utils import get_signed_data
 
 
+@login_required
 def send_sync_token(request):
     return render(request, 'sync/login.html', {'auth_data': get_signed_data(get_tokens_for_user(request.user))})
