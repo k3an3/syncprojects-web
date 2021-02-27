3700c7cdbb817ab2050e09b39cd2c91d652ec9a4
Keane O'Kelley <me@keane.space>
2021-02-26 22:42:33 -0500

    Add those migrations

--- core/models.py
+++ core/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
     image = models.ImageField(null=True, blank=True)
-    directory_name = models.CharField(max_length=200)
+    directory_name = models.CharField(max_length=200, null=True, blank=True)
     sync_enabled = models.BooleanField(default=True)
 
     def __str__(self):
