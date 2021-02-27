a0b496606703e86a0caccd55ea4733e50e423adc
Keane O'Kelley <me@keane.space>
2021-02-26 22:40:07 -0500

    Add extra fields to project model

--- core/models.py
+++ core/models.py
@@ -17,6 +17,8 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
     image = models.ImageField(null=True, blank=True)
+    directory_name = models.CharField(max_length=200)
+    sync_enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
