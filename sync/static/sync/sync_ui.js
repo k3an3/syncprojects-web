8a7fc4e44d508a7b83b9a20f0cfb042e8e5882c5
Studio Helper <kostudiohelper@gmail.com>
2021-03-13 14:23:48 -0500

    Fix client logging

--- sync/static/sync/sync_ui.js
+++ sync/static/sync/sync_ui.js
@@ -1,5 +1,6 @@
 const proj_re = /projects\/(?<project>[0-9]+)\/(songs\/(?<song>[0-9]+)\/)?/;
 const sync_button = document.querySelector('#sync_button');
+let sync_in_progress = false;
 
 function getContext() {
     let matches = window.location.pathname.match(proj_re);
@@ -11,34 +12,42 @@ function getContext() {
 
 sync_button.addEventListener('click', async event => {
     sync_button.textContent = "Syncing..."
+    sync_button.disabled = true;
     let context = getContext();
+    console.log("Got context");
     console.log(context);
     let result = null;
     if (context.project == null) {
         // Sync all projects
         let projects = await getProjects();
         let msg = {'projects': projects}
+        console.log("Syncing projects");
         console.log(msg);
         let signed = await signData(msg);
+        console.log("Signed data");
         console.log(signed);
         result = await startSync(signed);
     } else if (context.song == null) {
         // Sync single project
         let msg = {'projects': [context.project]};
+        console.log("Syncing project");
         console.log(msg);
         let signed = await signData(msg);
+        console.log("Signed data");
         console.log(signed);
         result = await startSync(signed);
     } else {
         // Sync single song
-        let msg = {'songs': [context.song]};
+        let msg = {'songs': [{'song': context.song, 'project': context.project}]};
+        console.log("Syncing song")
         console.log(msg);
         let signed = await signData(msg);
+        console.log("Signed data");
         console.log(signed);
         result = await startSync(signed);
     }
+    console.log("Got result");
     console.log(result);
-    
 });
 
 async function checkConnection() {
@@ -50,9 +59,11 @@ async function checkConnection() {
     });
     if (result != null && result.result == "pong") {
         console.log("Got PONG from server: " + result.task_id);
-        sync_button.className = "btn btn-sm btn-primary";
-        sync_button.textContent = "Sync";
-        sync_button.disabled = false;
+        if (!sync_in_progress) {
+            sync_button.className = "btn btn-sm btn-primary";
+            sync_button.textContent = "Sync";
+            sync_button.disabled = false;
+        }
     }
 }
 
