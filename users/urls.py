173a2ee5c4a1009596a3aeeebd7590dd613517b8
Keane O'Kelley <me@keane.space>
2021-06-13 11:38:43 -0400

    User profile updating

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from users import views
 app_name = 'users'
 urlpatterns = [
     path('profile/', views.UserDetailView.as_view(), name='user-profile'),
+    path('profile/edit/', views.UserUpdateView.as_view(), name='user-update'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse
 from django.views import generic
 
 
@@ -11,3 +12,20 @@ class UserDetailView(LoginRequiredMixin, generic.DetailView):
 
     def get_object(self, queryset=None):
         return get_user_model().objects.get(id=self.kwargs['pk']) if 'pk' in self.kwargs else self.request.user
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    template_name_suffix = '_update_form'
+    model = get_user_model()
+    fields = ['email', 'profile_picture', 'bio',
+              'instruments', 'genres_musical_taste', 'open_to_collaboration',
+              'private']
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('users:user-profile', kwargs={'pk': self.get_object().id})
