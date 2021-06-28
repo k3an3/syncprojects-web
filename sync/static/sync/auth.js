4481a1b15ea31e239eec4e896720c4e3a8cc72d8
Keane O'Kelley <keane.m.okelley@gmail.com>
2021-06-27 22:21:48 -0400

    Auth retry sleep

--- sync/static/sync/auth.js
+++ sync/static/sync/auth.js
@@ -1,11 +1,16 @@
 const auth_token = document.querySelector("#auth_token");
 const redirect_url = document.querySelector("#redirect_url");
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const do_login = async (n = 3) => {
     try {
         return await localRequest('auth', 'POST', {'data': auth_token.value}, false, false);
     } catch (err) {
         if (n === 1) throw err;
+        await timeout(1000);
         return await do_login(n - 1);
     }
 };
