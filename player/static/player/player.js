fbb50c3b0eb1609428870978139685f37bdc29e1
Keane O'Kelley <me@keane.space>
2021-08-15 02:02:01 -0400

    More player additions

--- player/static/player/player.js
+++ player/static/player/player.js
@@ -1,12 +1,21 @@
 const songUrl = document.getElementById('song_url').innerText;
 const waveformDiv = document.getElementById('waveform');
 const playerControls = document.getElementById('player-controls');
+const playerVolume = document.getElementById('player-volume');
+const volInc = 0.1;
+const seekInc = 1;
+const volMax = 1;
+const volMin = 0;
 
 let wavesurfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: 'cyan',
     progressColor: 'blue',
     hideScrollbar: true,
+    plugins: [
+        WaveSurfer.regions.create({}),
+        WaveSurfer.markers.create({}),
+    ]
 });
 
 wavesurfer.on('ready', function () {
@@ -19,9 +28,14 @@ wavesurfer.on('ready', function () {
 // noinspection JSUnresolvedVariable
 wavesurfer.load(songUrl);
 
+function updateVolume(vol) {
+    playerVolume.innerText = Math.round(vol * 100).toString() + "%";
+}
+
 async function playerControl(e) {
     const action = e.currentTarget.id.split("-")[1];
     console.log("Wavesurfer action " + action);
+    let vol = 0;
 
     switch (action) {
         case "play":
@@ -35,12 +49,47 @@ async function playerControl(e) {
             }
             break;
         case "forward":
-            wavesurfer.skipForward(1);
+            wavesurfer.skipForward(seekInc);
             break;
         case "backward":
-            wavesurfer.skipBackward(1);
+            wavesurfer.skipBackward(seekInc);
             break;
+        case "begin":
+            wavesurfer.seekTo(0);
+            break;
+        case "end":
+            wavesurfer.seekTo(1);
+            break;
+        case "loop":
 
+        case "volup":
+            vol = wavesurfer.getVolume();
+            if (vol + volInc <= volMax) {
+                vol += volInc;
+                wavesurfer.setVolume(vol);
+                updateVolume(vol);
+            } else {
+                wavesurfer.setVolume(volMax);
+            }
+            break;
+        case "voldown":
+            vol = wavesurfer.getVolume();
+            if (vol - volInc >= volMin) {
+                vol -= volInc;
+                wavesurfer.setVolume(vol);
+                updateVolume(vol);
+            } else {
+                wavesurfer.setVolume(volMin);
+            }
+            break;
+        case "mute":
+            wavesurfer.setMute(!wavesurfer.getMute());
+            if (wavesurfer.getMute()) {
+                e.target.className = "btn btn-danger";
+            } else {
+                e.target.className = "btn btn-outline-danger";
+            }
+            break;
     }
 }
 
@@ -50,5 +99,8 @@ wavesurfer.on('pause', () => {
 wavesurfer.on('play', () => {
 });
 
+wavesurfer.on('error', (e) => {
+    showToast("Audio Player", "Error: " + e, "danger");
+});
 
 bindEventToSelector('.player-control', playerControl);
