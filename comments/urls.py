cf47fd4b0708fa3309d59ef75df75b1360fa1d80
Keane O'Kelley <me@keane.space>
2021-08-25 23:38:50 -0400

    Probably fixed migrations without data loss

--- comments/urls.py
+++ comments/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-import comments
+from comments import views
 
 app_name = 'comments'
 urlpatterns = [
-    path('projects/<int:proj>/comment/', comments.views.CommentCreateView.as_view(), name='comment-create'),
-    path('projects/<int:proj>/songs/<int:song>/comment/', comments.views.CommentCreateView.as_view(),
+    path('projects/<int:proj>/comment/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('projects/<int:proj>/songs/<int:song>/comment/', views.CommentCreateView.as_view(),
          name='comment-create'),
-    path('projects/<int:proj>/songs/<int:song>/comment/<int:pk>/delete/', comments.views.CommentDeleteView.as_view(),
+    path('projects/<int:proj>/songs/<int:song>/comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),
          name='comment-delete'),
-    path('projects/<int:proj>/comment/<int:pk>/delete/', comments.views.CommentDeleteView.as_view(),
+    path('projects/<int:proj>/comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),
          name='comment-delete'),
 ]
