2363b17d6ba3d2321de0b4d2d74a127fd5e02df3
Keane O'Kelley <me@keane.space>
2021-10-23 15:41:36 -0400

    Add web sentry support, correctly tag this time

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 from syncprojectsweb.settings import SENTRY_JS_SCRIPT
 
@@ -6,5 +7,6 @@ register = template.Library()
 
 
 @register.simple_tag
+@mark_safe
 def sentry_js_script():
     return SENTRY_JS_SCRIPT
