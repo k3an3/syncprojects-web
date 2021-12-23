fb92824cebeeb3fef392dfa8c75d4eeaef4cbabe
Keane O'Kelley <me@keane.space>
2021-12-23 10:19:54 -0500

    Make version of syncprojects client download page that only shows changes

--- sync/views.py
+++ sync/views.py
@@ -28,6 +28,7 @@ class DownloadIndexView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['client_changes'] = ClientFeatureChangelog.objects.all().order_by('-id')
+        context['show_dl'] = not self.request.GET.get('show_dl') == '0'
         return context
 
 
